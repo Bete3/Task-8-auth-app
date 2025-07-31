5d8676c334fa8b1f0253edfe7378109d98121752
Betelhem Yehuala <167322606+Bete3@users.noreply.github.com>
2025-07-31 17:05:15 +0300

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  // Any other Next.js configuration options would go here
+};
+
+export default nextConfig;
